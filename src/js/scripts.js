837d14d17ac4fd50cdc126f8fbec0c5575d5f922
Lysi <amiragyan.lus@yandex.ru>
2017-10-30 11:51:37 +0300

    Add js click function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,35 @@
 // Your code here...
 
-// var optionElements = document.querySelectorAll('.option-content__item');
-//
-// for (var i = 0; i < optionElements.length; i++) {
-//     optionElements[i].addEventListener('click', function () {
-//         data = this.dataset;
-//
-//     });
-//
-// }
+var formProduct = document.getElementById('form-product');
+const optionClickClass = 'option_click';
+
+formProduct.addEventListener('click', function (event) {
+    var target = event.target;
+    while (target != this) {
+        if (target.className == 'option-content__item') {
+            var targetBlock = findOptionBlock(target);
+            var selectOptions = targetBlock.getElementsByClassName(optionClickClass);
+            for (var i = 0; i < selectOptions.length; i++) {
+                selectOptions[i].classList.remove(optionClickClass);
+            }
+
+            selectOption(target);
+            return;
+        }
+        target = target.parentNode;
+    }
+});
+function selectOption(node) {
+    data = node.dataset;
+    var option = node.getElementsByClassName('option')[0];
+    option.classList.add(optionClickClass);
+}
+function findOptionBlock(option) {
+    var optionBlock = option.parentNode ;
+    while (optionBlock != formProduct) {
+        if (optionBlock.classList.contains('product-option__content')) {
+            return optionBlock;
+        }
+        optionBlock = optionBlock.parentNode;
+    }
+}
